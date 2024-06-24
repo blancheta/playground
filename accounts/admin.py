6ff8dda0a4f7ba93193fb81429306f8f3e8e4cee
aldazar <alexandreblanchet44@gmail.com>
2024-06-24 11:59:43 +0100

    fix: add custom group

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.contrib.auth.models import Group
+
+from accounts.models import Group as NewGroup
+
+class GroupAdmin(ModelAdmin):
+
+    fields = ("name", "permissions")
+
 
 # Register your models here.
+admin.site.register(NewGroup, GroupAdmin)
+admin.site.unregister(Group)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import CharField
+from django_group_model.models import AbstractGroup
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    groups = models.ManyToManyField(
+        'accounts.Group',
+        verbose_name="groups",
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_name="user_set",
+        related_query_name="user",
+
+    )
+
+class Group(AbstractGroup):
+    # You can add custom fields here
+    name = CharField(max_length=50, unique=False)
